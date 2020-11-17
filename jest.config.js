6963470b6c7639a7041e85e03d9fb858bc3abe17
pablo <pjcalvov@gmail.com>
2020-11-17 14:27:14 -0600

    added codecoverage report

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  // preset: '@vue/cli-plugin-unit-jest',
   globals: {},
   testMatch: ["**.spec.js"],
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: -10
+    }
+  },
+  collectCoverageFrom: [
+    "lib/**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
 };
--- test/commands/screenshot.spec.js
+++ test/commands/screenshot.spec.js
@@ -1,4 +1,91 @@
-test('verify', async() => {
+const colors = require('colors');
 
-  expect(1).toBe(1);
+
+jest.mock('../../lib/tasks/collect-screenshots', () => taskCollectScreenshot = jest.fn());
+
+const screenshot = require('../../lib/commands/screenshot');
+
+// create mocked function 
+taskCollectScreenshot.mockImplementation((options) => {  
+  return {
+    title: `Mock step`,
+    task: () => {
+        console.log(`Step executed with: ${options.browserType}`)
+    }
+  }
+});
+
+describe('screenshots-command', () => {
+
+  test('test no browser provided defaults to chrome', async() => {
+
+    const consoleSpy0 = jest.spyOn(console, 'log');
+
+    const options = {
+      url:'https://www.google.com'
+    }
+    
+    await screenshot(options);
+    
+    expect(consoleSpy0).toHaveBeenNthCalledWith(2, 'Step executed with: chromium');
+
+  });
+
+  test('test browser provided use provided values', async() => {
+
+    const consoleSpy1 = jest.spyOn(console, 'log');
+    
+    const options = {
+      url:'https://www.google.com',
+      browsers: ['firefox','chromium']
+    }
+    
+    await screenshot(options);
+    
+    expect(consoleSpy1).toHaveBeenNthCalledWith(2, 'Step executed with: firefox');
+    expect(consoleSpy1).toHaveBeenNthCalledWith(3, 'Step executed with: chromium');
+
+  });
+
+  test('test browser provided use provided values', async() => {
+
+    const consoleSpy3 = jest.spyOn(console, 'log');
+    
+    const options = {
+      url:'asda.com',
+    }
+    
+    await screenshot(options);
+    
+    expect(consoleSpy3).toHaveBeenCalledTimes(1)
+
+  });
+
+  test('test when task returns error', async() => {
+
+    taskCollectScreenshot.mockImplementation((options) => {  
+      return {
+        title: `Mock Error Step`,
+        task: () => {
+            throw new Error('error')
+        }
+      }
+    });
+
+    const consoleSpy5 = jest.spyOn(console, 'log');
+    
+    const options = {
+      url:'https://www.google.com',
+    }
+    
+    await screenshot(options);
+    
+    expect(consoleSpy5).toHaveBeenNthCalledWith(2, colors.bgWhite(colors.red('error')),' Something went wrong. Please check the specified parameters.');
+
+  });
+
+  afterEach(() => {    
+    jest.restoreAllMocks();
+  });
+
 });
